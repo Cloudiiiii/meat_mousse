30a5b3547f04871c5bfcabac0af89644fc93ea4f
Cloudiiiii <Yusufhafeji33@gmail.com>
2024-06-06 19:43:39 -0400

    react set up

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-function App() {
-  return <h1>Hello from the App component</h1>;
-}
-
-ReactDOM.createRoot(document.getElementById("app")).render(<App />);
